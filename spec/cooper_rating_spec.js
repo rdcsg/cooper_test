c41157e6f5adc2a76dcd7dbdabd312ff41d2e309
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-08-10 23:18:20 +0200

    make the implementation and have the src pass, but the spec fail

--- spec/cooper_rating_spec.js
+++ spec/cooper_rating_spec.js
@@ -2,11 +2,12 @@ describe("CooperRating", function() {
 
   beforeEach(function() {
     person = new Person ({ selected_gender: "male", age: 21, distance_performed: 1980 });
+    //what_is_the = new CooperRating();
   });
 
   it("finds the cooper test rating to be \"Bad\"", function() {
-    person = new Person ({ selected_gender: "male", age: 21, distance_performed: 1980, rating_of: "Bad" });
-    //what_is_the.rating();
+    what_is_the = new CooperRating();
+    what_is_the.rating();
     expect(person.rating_of).toEqual("Bad");
   });
 
---
+++ src/cooper_rating.js
@@ -0,0 +1,13 @@
+function CooperRating() {
+
+}
+
+CooperRating.prototype.rating = function(obj) {
+  var rating = obj.rating;
+  matchRating();
+};
+
+
+function matchRating(obj) {
+  this.rating = "Bad";
+}
