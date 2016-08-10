60e30b82770ef60f4c443ce880373d8c1851c20e
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-08-10 18:13:17 +0200

    add first test file of Person object

---
+++ spec/person_spec.js
@@ -0,0 +1,19 @@
+describe ("Person", function() {
+
+  //set gender, age and performance
+  beforeEach(function() {
+    person = new Person ({age: 21, selected_gender: "male", distance_performed: 1980 });
+  });
+
+  it("has the selected gender to \'male\' ", function() {
+    expect(person.selected_gender).toEqual("male");
+  });
+
+  it("has the age of 21", function() {
+    expect(person.age).toEqual(21);
+  });
+
+  it("has the result of 1980 metres run", function() {
+    expect(person.distance_performed).toEqual(1980);
+  });
+});
