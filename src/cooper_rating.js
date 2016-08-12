7377164f956d7a8b7a492e4effe57e5540dcf8d6
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-08-12 20:41:15 +0200

    determines agegroups and achievements

--- src/cooper_rating.js
+++ src/cooper_rating.js
@@ -2,41 +2,103 @@ function rating_of(obj) {
   var gender = obj.selected_gender;
   var age = obj.age;
   var achievement = obj.distance_performed;
-  var rating = obj.rating;
-
-  // choose database depending on gender
-  if (gender == "female")
-    obj.rating = RateFemale(age, achievement);
-  else
-    obj.rating = RateMale(age, achievement);
+  var agegroup = determineAgeGroup(age);
+  obj.rating = rate(agegroup, gender, achievement);
 }
-
-function RateMale(age, achievement) {
-  var rating = "Bad";
-  return rating;
+function determineAgeGroup(age) {
+  if ( age >= 20 ) {
+    for (i = 5; i >=2 ; i--) {
+      if (age >= i*10) agegroup = i+2;
+    }
+  }
+  else {
+    if (age >= 15) {
+     if (age >= 17)
+      agegroup = 3;
+     agegroup = 2;
+    }
+    agegroup = 1;
+  }
 }
-
-function RateFemale(age, achievement) {
-  var rating = "Good";
-  return rating;
+function rate(ag, g, ac) {
+  var s = "";
+  var a = ["Excellent", "Above Average", "Average", "Below Average", "Poor"];
+  if (ag == 7) {
+      if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[0];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[2];
+      else s = a[3];
+  }
+  if (ag == 6) {
+      if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[0];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[2];
+      else s = a[3];
+  }
+  if (ag == 5) {
+      if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[0];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[2];
+      else s = a[3];
+  }
+  if (ag == 4) {
+      if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[0];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[2];
+      else s = a[3];
+  }
+  if (ag == 3) {
+      if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[0];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[2];
+      else s = a[3];
+  }
+  if (ag == 2) {
+      if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[0];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[2];
+      else s = a[3];
+  }
+  if (ag == 1) {
+      if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[0];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[1];
+      else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) )
+        s = a[2];
+      else s = a[3];
+  }
+  return s;
 }
-
-
-// Which solution is the best?
-// alt 1, do ifs just testing for one interval endpoint value (the smaller one) for each age grp
-// start from the highest interval and going down to the lowest - this seems to be the fastes code
-// nest the if statements with elses , do not nest inside other ifs
-
-// alt 2, create dbl arrays and call for the smaller value in each column with a switch
-// but then you have to include both interval endpoint values
-
-// var myArray = new Array();
-// myArray[row][column] = value;
-// var thirteen_fourteen_interval[”13-14”][0] = 2000;
-
-// the dbl array solution relies on the array being create and the valued assigned
-// before you can check the values in a switch-case
-// which means that from a rapid workflow perspective it adds an extra step of assigning
-// the necessary values to the array. In contrast, the if-statement-solution just needs
-// to have the assigning of values in the if-statement itself. that makes this workflow
-// much faster as the assigning and conditional check takes place at the same time
