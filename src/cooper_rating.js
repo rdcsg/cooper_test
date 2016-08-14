023a51ad3152f8cbe2900309d892f5cb31bb09d1
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-08-14 09:53:18 +0200

    cleans up unnecessary age groups

--- src/cooper_rating.js
+++ src/cooper_rating.js
@@ -2,57 +2,47 @@ function rating_of(obj) {
   var gender = obj.selected_gender;
   var age = obj.age;
   var achievement = obj.distance_performed;
-  var agegroup = determineAgeGroup(age);
-  obj.rating = rate(agegroup, gender, achievement);
+  obj.rating = rate(age, gender, achievement);
 }
-function determineAgeGroup(age) {
-  if( age >= 50 ) agegroup = 7;
-  else if( age >= 40 ) agegroup = 6;
-  else if( age >= 30 ) agegroup = 5;
-  else if( age >= 20 ) agegroup = 4;
-  else if( age >= 17 ) agegroup = 3;
-  else if( age >= 15 ) agegroup = 2;
-  else agegroup = 1;
-}
-function rate(ag, g, ac) {
+function rate(age, g, ac) {
   var s = "";
-  if (ag == 7) {
+  if ( age >= 50 ) {
       if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) ) s = "Excellent";
       else if ( ( g == "male" && ac > 2000) || ( g == "female" && ac > 1700 ) ) s = "Above Average";
       else if ( ( g == "male" && ac > 1600) || ( g == "female" && ac > 1400 ) ) s = "Average";
       else if ( ( g == "male" && ac > 1300) || ( g == "female" && ac > 1100 ) ) s = "Below Average";
   }
-  if (ag == 6) {
+  if ( age >= 40 ) {
     if ( ( g == "male" && ac > 2500) || ( g == "female" && ac > 2300 ) ) s = "Excellent";
     else if ( ( g == "male" && ac > 2100) || ( g == "female" && ac > 1900 ) ) s = "Above Average";
     else if ( ( g == "male" && ac > 1700) || ( g == "female" && ac > 1500 ) ) s = "Average";
     else if ( ( g == "male" && ac > 1400) || ( g == "female" && ac > 1200 ) ) s = "Below Average";
   }
-  if (ag == 5) {
+  if ( age >= 30 ) {
     if ( ( g == "male" && ac > 2700) || ( g == "female" && ac > 2500 ) ) s = "Excellent";
     else if ( ( g == "male" && ac > 2300) || ( g == "female" && ac > 2000 ) ) s = "Above Average";
     else if ( ( g == "male" && ac > 1900) || ( g == "female" && ac > 1700 ) ) s = "Average";
     else if ( ( g == "male" && ac > 1500) || ( g == "female" && ac > 1400 ) ) s = "Below Average";
   }
-  if (ag == 4) {
+  if ( age >= 20 ) {
     if ( ( g == "male" && ac > 2800) || ( g == "female" && ac > 2700 ) ) s = "Excellent";
     else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) ) s = "Above Average";
     else if ( ( g == "male" && ac > 2200) || ( g == "female" && ac > 1800 ) ) s = "Average";
     else if ( ( g == "male" && ac > 1600) || ( g == "female" && ac > 1500 ) ) s = "Below Average";
   }
-  if (ag == 3) {
+  if ( age >= 17 ) {
     if ( ( g == "male" && ac > 3000) || ( g == "female" && ac > 2300 ) ) s = "Excellent";
     else if ( ( g == "male" && ac > 2700) || ( g == "female" && ac > 2100 ) ) s = "Above Average";
     else if ( ( g == "male" && ac > 2500) || ( g == "female" && ac > 1800 ) ) s = "Average";
     else if ( ( g == "male" && ac > 2300) || ( g == "female" && ac > 1700 ) ) s = "Below Average";
   }
-  if (ag == 2) {
+  if ( age >= 15 ) {
     if ( ( g == "male" && ac > 2800) || ( g == "female" && ac > 2100 ) ) s = "Excellent";
     else if ( ( g == "male" && ac > 2500) || ( g == "female" && ac > 2000 ) ) s = "Above Average";
     else if ( ( g == "male" && ac > 2300) || ( g == "female" && ac > 1700 ) ) s = "Average";
     else if ( ( g == "male" && ac > 2200) || ( g == "female" && ac > 1600 ) ) s = "Below Average";
   }
-  if (ag == 1) {
+  if ( age < 15 ) {
     if ( ( g == "male" && ac > 2700) || ( g == "female" && ac > 2000 ) ) s = "Excellent";
     else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 1900 ) ) s = "Above Average";
     else if ( ( g == "male" && ac > 2200) || ( g == "female" && ac > 1600 ) ) s = "Average";
