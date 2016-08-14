bd06af11ccd541f31f099efe01c8be3f7de671a5
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-08-14 10:16:48 +0200

    removes unnecessary extra variables

--- src/cooper_rating.js
+++ src/cooper_rating.js
@@ -2,51 +2,46 @@ function rating_of(obj) {
   var gender = obj.selected_gender;
   var age = obj.age;
   var achievement = obj.distance_performed;
-  obj.rating = rate(age, gender, achievement);
-}
-function rate(age, g, ac) {
-  var s = "";
   if ( age >= 50 ) {
-      if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) ) s = "Excellent";
-      else if ( ( g == "male" && ac > 2000) || ( g == "female" && ac > 1700 ) ) s = "Above Average";
-      else if ( ( g == "male" && ac > 1600) || ( g == "female" && ac > 1400 ) ) s = "Average";
-      else if ( ( g == "male" && ac > 1300) || ( g == "female" && ac > 1100 ) ) s = "Below Average";
+      if ( ( gender == "male" && achievement > 2400) || ( gender == "female" && achievement > 2200 ) ) obj.rating = "Excellent";
+      else if ( ( gender == "male" && achievement > 2000) || ( gender == "female" && achievement > 1700 ) ) obj.rating = "Above Average";
+      else if ( ( gender == "male" && achievement > 1600) || ( gender == "female" && achievement > 1400 ) ) obj.rating = "Average";
+      else if ( ( gender == "male" && achievement > 1300) || ( gender == "female" && achievement > 1100 ) ) obj.rating = "Below Average";
   }
   if ( age >= 40 ) {
-    if ( ( g == "male" && ac > 2500) || ( g == "female" && ac > 2300 ) ) s = "Excellent";
-    else if ( ( g == "male" && ac > 2100) || ( g == "female" && ac > 1900 ) ) s = "Above Average";
-    else if ( ( g == "male" && ac > 1700) || ( g == "female" && ac > 1500 ) ) s = "Average";
-    else if ( ( g == "male" && ac > 1400) || ( g == "female" && ac > 1200 ) ) s = "Below Average";
+    if ( ( gender == "male" && achievement > 2500) || ( gender == "female" && achievement > 2300 ) ) obj.rating = "Excellent";
+    else if ( ( gender == "male" && achievement > 2100) || ( gender == "female" && achievement > 1900 ) ) obj.rating = "Above Average";
+    else if ( ( gender == "male" && achievement > 1700) || ( gender == "female" && achievement > 1500 ) ) obj.rating = "Average";
+    else if ( ( gender == "male" && achievement > 1400) || ( gender == "female" && achievement > 1200 ) ) obj.rating = "Below Average";
   }
   if ( age >= 30 ) {
-    if ( ( g == "male" && ac > 2700) || ( g == "female" && ac > 2500 ) ) s = "Excellent";
-    else if ( ( g == "male" && ac > 2300) || ( g == "female" && ac > 2000 ) ) s = "Above Average";
-    else if ( ( g == "male" && ac > 1900) || ( g == "female" && ac > 1700 ) ) s = "Average";
-    else if ( ( g == "male" && ac > 1500) || ( g == "female" && ac > 1400 ) ) s = "Below Average";
+    if ( ( gender == "male" && achievement > 2700) || ( gender == "female" && achievement > 2500 ) ) obj.rating = "Excellent";
+    else if ( ( gender == "male" && achievement > 2300) || ( gender == "female" && achievement > 2000 ) ) obj.rating = "Above Average";
+    else if ( ( gender == "male" && achievement > 1900) || ( gender == "female" && achievement > 1700 ) ) obj.rating = "Average";
+    else if ( ( gender == "male" && achievement > 1500) || ( gender == "female" && achievement > 1400 ) ) obj.rating = "Below Average";
   }
   if ( age >= 20 ) {
-    if ( ( g == "male" && ac > 2800) || ( g == "female" && ac > 2700 ) ) s = "Excellent";
-    else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 2200 ) ) s = "Above Average";
-    else if ( ( g == "male" && ac > 2200) || ( g == "female" && ac > 1800 ) ) s = "Average";
-    else if ( ( g == "male" && ac > 1600) || ( g == "female" && ac > 1500 ) ) s = "Below Average";
+    if ( ( gender == "male" && achievement > 2800) || ( gender == "female" && achievement > 2700 ) ) obj.rating = "Excellent";
+    else if ( ( gender == "male" && achievement > 2400) || ( gender == "female" && achievement > 2200 ) ) obj.rating = "Above Average";
+    else if ( ( gender == "male" && achievement > 2200) || ( gender == "female" && achievement > 1800 ) ) obj.rating = "Average";
+    else if ( ( gender == "male" && achievement > 1600) || ( gender == "female" && achievement > 1500 ) ) obj.rating = "Below Average";
   }
   if ( age >= 17 ) {
-    if ( ( g == "male" && ac > 3000) || ( g == "female" && ac > 2300 ) ) s = "Excellent";
-    else if ( ( g == "male" && ac > 2700) || ( g == "female" && ac > 2100 ) ) s = "Above Average";
-    else if ( ( g == "male" && ac > 2500) || ( g == "female" && ac > 1800 ) ) s = "Average";
-    else if ( ( g == "male" && ac > 2300) || ( g == "female" && ac > 1700 ) ) s = "Below Average";
+    if ( ( gender == "male" && achievement > 3000) || ( gender == "female" && achievement > 2300 ) ) obj.rating = "Excellent";
+    else if ( ( gender == "male" && achievement > 2700) || ( gender == "female" && achievement > 2100 ) ) obj.rating = "Above Average";
+    else if ( ( gender == "male" && achievement > 2500) || ( gender == "female" && achievement > 1800 ) ) obj.rating = "Average";
+    else if ( ( gender == "male" && achievement > 2300) || ( gender == "female" && achievement > 1700 ) ) obj.rating = "Below Average";
   }
   if ( age >= 15 ) {
-    if ( ( g == "male" && ac > 2800) || ( g == "female" && ac > 2100 ) ) s = "Excellent";
-    else if ( ( g == "male" && ac > 2500) || ( g == "female" && ac > 2000 ) ) s = "Above Average";
-    else if ( ( g == "male" && ac > 2300) || ( g == "female" && ac > 1700 ) ) s = "Average";
-    else if ( ( g == "male" && ac > 2200) || ( g == "female" && ac > 1600 ) ) s = "Below Average";
+    if ( ( gender == "male" && achievement > 2800) || ( gender == "female" && achievement > 2100 ) ) obj.rating = "Excellent";
+    else if ( ( gender == "male" && achievement > 2500) || ( gender == "female" && achievement > 2000 ) ) obj.rating = "Above Average";
+    else if ( ( gender == "male" && achievement > 2300) || ( gender == "female" && achievement > 1700 ) ) obj.rating = "Average";
+    else if ( ( gender == "male" && achievement > 2200) || ( gender == "female" && achievement > 1600 ) ) obj.rating = "Below Average";
   }
   if ( age < 15 ) {
-    if ( ( g == "male" && ac > 2700) || ( g == "female" && ac > 2000 ) ) s = "Excellent";
-    else if ( ( g == "male" && ac > 2400) || ( g == "female" && ac > 1900 ) ) s = "Above Average";
-    else if ( ( g == "male" && ac > 2200) || ( g == "female" && ac > 1600 ) ) s = "Average";
-    else if ( ( g == "male" && ac > 2100) || ( g == "female" && ac > 1500 ) ) s = "Below Average";
-  } else s = "Poor";
-  return s;
+    if ( ( gender == "male" && achievement > 2700) || ( gender == "female" && achievement > 2000 ) ) obj.rating = "Excellent";
+    else if ( ( gender == "male" && achievement > 2400) || ( gender == "female" && achievement > 1900 ) ) obj.rating = "Above Average";
+    else if ( ( gender == "male" && achievement > 2200) || ( gender == "female" && achievement > 1600 ) ) obj.rating = "Average";
+    else if ( ( gender == "male" && achievement > 2100) || ( gender == "female" && achievement > 1500 ) ) obj.rating = "Below Average";
+  } else obj.rating = "Poor";
 }
